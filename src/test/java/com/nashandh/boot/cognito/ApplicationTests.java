66286df825bb11c9f4f365ce538f686658408fa6
Andrew Nash <nash.andrewh@gmail.com>
2024-04-30 21:24:18 -0400

    Initial commit

---
+++ src/test/java/com/nashandh/boot/cognito/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashandh.boot.cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
